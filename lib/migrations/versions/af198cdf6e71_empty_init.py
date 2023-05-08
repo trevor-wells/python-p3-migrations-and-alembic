7d64bbcce90ef03508e52ee427a96fd8c81b3966
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-07 18:02:11 -0600

    final commit

---
+++ lib/migrations/versions/af198cdf6e71_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: af198cdf6e71
+Revises: 
+Create Date: 2023-05-07 11:48:01.002708
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'af198cdf6e71'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
